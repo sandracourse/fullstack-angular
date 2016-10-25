149dd7d3c7eee5b59f39c3a847ee39b3549a335b
sandracourse <sl1210@columbia.edu>
2016-10-24 22:08:15 -0400

    New files

---
+++ assignment1-solution/app.js
@@ -0,0 +1,40 @@
+(function() {
+    'use strict';
+
+    angular.module('LunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController($scope) {
+        $scope.msg = "";
+        $scope.checkLunch = function() {
+            var dishArray = splitString($scope.dishes, ',');
+            dishArray = dishArray.filter(Boolean);
+
+            var dishLength = dishArray.length;
+            if (dishLength == 0) {
+                $scope.msg = "Please enter data first.";
+                $scope.divClass = "has-error";
+                $scope.msgClass = "error";
+            } else if (dishLength <= 3) {
+                $scope.msg = "Enjoy!";
+                $scope.divClass = "has-success";
+                $scope.msgClass = "success";
+            } else {
+                $scope.msg = "Too much!";
+                $scope.divClass = "has-success";
+                $scope.msgClass = "success";
+            }
+        };
+    }
+
+    function splitString(stringToSplit, separator) {
+        if (stringToSplit) {
+            return stringToSplit.split(separator);
+        } else {
+            return [];
+        }
+    }
+
+})();
